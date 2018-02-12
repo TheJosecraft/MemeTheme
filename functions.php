eaf16aa1006ddf8d5a6888534f86ab38c1cb4835
German Menendez <gemebe@hotmail.es>
2018-02-12 12:27:03 +0100

    añadida función mis_menus

--- functions.php
+++ functions.php
@@ -1,1 +1,15 @@
-functions.php
+<?php
+
+/**
+ * Crear nuestros menús gestionables desde el
+ * administrador de Wordpress.
+ */
+
+function mis_menus() {
+  register_nav_menus(
+    array(
+      'navegation' => __( 'Menú de navegación' ),
+    )
+  );
+}
+add_action( 'init', 'mis_menus' );
