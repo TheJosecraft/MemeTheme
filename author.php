9e5bf3b6cc569a92927d6a0a913e94fd0253d59f
German	Menendez <gemebe@hotmail.es>
2018-02-12 13:16:47 +0100

    autohor.php

---
+++ author.php
@@ -0,0 +1,30 @@
+<?php get_header(); ?>
+<!-- Autor -->
+<p>Posts de <strong><?php echo get_the_author(); ?></strong></p>
+<!-- Posts -->
+<?php if (have_posts()) : ?>
+	<section>
+		<?php while(have_posts()) : the_post(); ?>
+	<article>
+		<header>
+			<h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+			<time datatime="<?php the_time( 'Y-m-j' ); ?>"><?php the_time( 'j F, Y' ); ?></time>
+		</header>
+		<?php the_excerpt(); ?>
+		<footer>
+			<address>Por <?php the_author_posts_link(); ?></address>
+		</footer>
+	</article>
+<?php endwhile; ?>
+<div class="pagination">
+	<span class="in-left"><?php next_posts_link('<< Entradas antiguas'); ?></span>
+	<span class="in-right"><?php previous_posts_link('Entradas más recientes >>'); ?></span>
+</div>
+	</section>
+<?php else : ?>
+<p><?php _e( 'Ups!, no hay entradas.' ); ?></p>
+<?php endif; ?>
+<!-- sidebar -->
+<?php get_sidebar(); ?>
+<!-- footer -->
+<?php get_footer(); ?>
