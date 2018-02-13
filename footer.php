fc538e2f075b7ad5cdcbdae594def772bba30f4a
José Carlos <josekawa1996@gmail.com>
2018-02-13 12:09:47 +0100

    Footer

--- footer.php
+++ footer.php
@@ -1,6 +1,19 @@
+  <div class="container">
+  	<div class="row">
+  	<div class="col-lg-3 col-md-6">
+  		<h3>Categorías</h3>
+  		<?php echo get_the_category_list(); ?>
+  	</div>
+  	<div class="col-lg-3 col-md-6">
+  		<h3>Contacto:</h3>
+  		<p>¿Alguna duda? No dudes en contactarnos</p>
+  		<p><a href="">Contáctanos</a></p>
+  	</div>
+  </div>
   <footer>
-       <small>InboundCycle © <?php echo date("Y") ?></small>
+       <small>EAG © <?php echo date("Y") ?></small>
     </footer>
     <?php wp_footer(); ?>
+  </div>
   </body>
   </html>
