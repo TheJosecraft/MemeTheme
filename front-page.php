c3fba70193b9460647e9c22072bd0a3f7931ffe9
José Carlos <josekawa1996@gmail.com>
2018-02-19 12:05:24 +0100

    Imagen

--- front-page.php
+++ front-page.php
@@ -9,7 +9,7 @@
 <?php while(have_posts()) : the_post(); ?>
 
 	<div class="card text-white bg-primary mb-4">
-		<a href="<?php the_permalink(); ?>"><?php echo "<img class=card-img-top src=".the_post_thumbnail().">"?></a>
+		<a href="<?php the_permalink(); ?>"><?php echo "<img class='card-img-top img-fluid' src=".the_post_thumbnail().">"?></a>
 		<div class="card-body">
 			<h5 class="card-title"><a class="text-white" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
 		<time class="card-text text-white float-left" datatime="<?php the_time( 'Y-m-j' ); ?>"><?php the_time( 'j F, Y' ); ?></time>
