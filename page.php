3b6d303ea225f6e8b1f03532189c1539a7b008e2
José Carlos <josekawa1996@gmail.com>
2018-02-19 12:44:06 +0100

    Estructura páginas

--- page.php
+++ page.php
@@ -2,18 +2,17 @@
 <?php get_header(); ?>
 <!-- Contenido de página de inicio -->
 <?php if ( have_posts() ) : the_post(); ?>
-<div class="container">
+<div class="container mt-4">
 	<div class="row">
-		<section class="col-md-12">
+		<section class="col-md-8">
     		<h1><?php the_title(); ?></h1>
     		<?php the_author_posts_link(); ?>
     		<?php the_excerpt(); ?>
 		</section>
-	</div>
-</div>
-  
-<?php endif; ?>
+		<?php endif; ?>
 <!-- Archivo de barra lateral por defecto -->
 <?php get_sidebar(); ?>
 <!-- Archivo de pié global de Wordpress -->
 <?php get_footer(); ?>
+	</div>
+</div>
