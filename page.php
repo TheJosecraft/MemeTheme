70fb96311e6cdaa5d7125e4cea2ac324f0e3018c
José Carlos <josekawa1996@gmail.com>
2018-02-19 13:22:10 +0100

    Formulario busqueda

--- page.php
+++ page.php
@@ -1,15 +1,18 @@
 <!-- Archivo de cabecera global de Wordpress -->
 <?php get_header(); ?>
 <!-- Contenido de página de inicio -->
-<?php if ( have_posts() ) : the_post(); ?>
 <div class="container mt-4">
-	<div class="row">
-		<section class="col-md-8">
+		<div class="row">
+			<div class="col-md-8">
+<?php if ( have_posts() ) : the_post(); ?>
+		<section>
     		<h1><?php the_title(); ?></h1>
     		<?php the_author_posts_link(); ?>
     		<?php the_excerpt(); ?>
+    		<?php echo get_the_ID(); ?>
 		</section>
 		<?php endif; ?>
+</div>
 <!-- Archivo de barra lateral por defecto -->
 <?php get_sidebar(); ?>
 <!-- Archivo de pié global de Wordpress -->
--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
 <form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
     <div class="input-group mb-4 col-md-9">
-        <input class="form-control" type="text" value="" name="s" id="s">
+        <input class="form-control busqueda" type="text" value="" name="s" id="s">
         <div class="input-group-append">
         	<button class="btn bg-primary" type="submit" id="searchsubmit" value="Search"><i class="fas fa-search"></i></button>
         </div>
