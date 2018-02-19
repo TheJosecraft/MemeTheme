ea0e0002d1c51390fb9d5b5db26ab3ed9aba8a9d
german menendez <gemebe@hotmail.es>
2018-02-19 12:25:51 +0100

    cambiada posicion de style.css

--- functions.php
+++ functions.php
@@ -19,9 +19,9 @@ add_action( 'init', 'mis_menus' );
 */
 
 function theme_styles(){
+	wp_enqueue_style('main_css', get_template_directory_uri() . 'style.css');
 	wp_enqueue_style( 'bootstrap_css', get_template_directory_uri() . '/css/bootstrap.min.css' );
-  wp_enqueue_style( 'fontawesome_css', get_template_directory_uri() . '/css/fontawesome-all.min.css' );
-	wp_enqueue_style('main_css', get_template_directory_uri() . '/style.css');
+  wp_enqueue_style( 'fontawesome_css', get_template_directory_uri() . '/css/fontawesome-all.min.css' );	
 }
 
 add_action('wp_enqueue_scripts', 'theme_styles');
